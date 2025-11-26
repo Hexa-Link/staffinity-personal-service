93bd28762c32682eaebe54976a511ca99c8e1fd1
WillmanGZ <willmang@uninorte.edu.co>
2025-11-26 17:50:50 -0500

    Feat: create resource not found exception

---
+++ src/Staffinity.Personal.Domain/Modules/Notifications/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Staffinity.Personal.Domain.Modules.Notifications.Exceptions
+{
+    internal class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
