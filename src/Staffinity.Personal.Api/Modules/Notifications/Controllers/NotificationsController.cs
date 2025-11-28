62a0827c9e888ae7ebfebc0c6243d0524bc1b41f
WillmanGZ <willmang@uninorte.edu.co>
2025-11-28 13:13:37 -0500

    Feat: Add Get Controller to notifications

---
+++ src/Staffinity.Personal.Api/Modules/Notifications/Controllers/NotificationsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Staffinity.Personal.Domain.Modules.Notifications.Exceptions;
+using Staffinity.Personal.Domain.Modules.Notifications.Ports.In;
+
+namespace Staffinity.Personal.Api.Modules.Notifications.Controllers
+{
+    [ApiController]
+    [Route("notifications")]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly IGetAllNotificationsUseCase _getAllNotificationsUseCase;
+
+        public NotificationsController(IGetAllNotificationsUseCase getAllNotificationsUseCase)
+        {
+            _getAllNotificationsUseCase = getAllNotificationsUseCase;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return StatusCode(200, _getAllNotificationsUseCase.GetAllAsync());
+            }
+            catch (ResourceNotFoundException ex)
+            {
+                return StatusCode(404, ex.Message);
+            }
+        }
+    }
+}
